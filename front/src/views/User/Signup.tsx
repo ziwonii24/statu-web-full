3158ffe2a60aebd4769a69b58d15053413c3ab7e
ziwonii24 <ziwonii25@gmail.com>
2020-01-30 16:37:44 +0900

    ADD :: 회원가입 간단한 폼

---
+++ front/src/views/User/Signup.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react'
+import SignupComponent from '../../components/User/Signup'
+
+
+const Signup: FunctionComponent = () => {
+  return <SignupComponent />
+}
+
+export default Signup
