068085ec9754255fc3019d724b409644dfde072d
ziwonii24 <ziwonii25@gmail.com>
2020-02-14 10:09:43 +0900

    bug

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router'
+import { Route, Switch, Redirect } from 'react-router'
 import NavBar from '../components/Nav/NavBar'
 import MainPage from '../pages/MainPage'
 import SignupPage from '../pages/SignupPage'
@@ -16,14 +16,15 @@ const routes = (
     <NavBar />
     <Switch>
       <Route exact path="/" component={MainPage} />
-      <Route path="/signup" component={SignupPage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/userinfo" component={UpdateUserInfoPage} />
-      <Route path="/plan/:userName" component={PlanPage} />
-      <Route path="/importedplan" component={ImportedPlanPage} />
-      <Route path="/search/:query" component={SearchResultPage} />
-      <Route path="/community" component={CommunityPage} />
-      <Route path="/detail/:planId" component={DetailPage} />
+      <Route exact path="/signup" component={SignupPage} />
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/userinfo" component={UpdateUserInfoPage} />
+      <Route exact path="/plan/:userName" component={PlanPage} />
+      <Route exact path="/importedplan" component={ImportedPlanPage} />
+      <Route exact path="/search/:query" component={SearchResultPage} />
+      <Route exact path="/community" component={CommunityPage} />
+      <Route exact path="/detail/:planId" component={DetailPage} />
+      <Redirect path="*" to="/" />
     </Switch>
   </div>
 )
