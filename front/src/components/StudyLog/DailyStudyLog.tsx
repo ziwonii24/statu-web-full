f4fb4a790525e73757b1291da5d72fdb8b10025f
sungwon <sungwoncho94@naver.com>
2020-02-19 16:20:55 +0900

    잔디 색 골라 & 파비콘 추가

--- front/src/components/StudyLog/DailyStudyLog.tsx
+++ front/src/components/StudyLog/DailyStudyLog.tsx
@@ -16,7 +16,16 @@ const DailyStudyLog: FunctionComponent<InterFace> = (props: InterFace) => {
 
   return <div
     className='dailyStudyLog third-color'
-    style={{ backgroundColor: progress >= 95 ? "rgb(0, 58, 0)" : (progress >= 75 ? "rgb(0, 107, 0)" : (progress >= 50 ? "rgb(0, 150, 25)" : (progress >= 25 ? "rgb(111, 211, 102)" : (progress > 0 ? "rgb(175, 226, 108)" : "rgb(211, 211, 211)"))))}}
+    // 초록버전
+    // style={{ backgroundColor: progress >= 95 ? "rgb(17, 73, 17)" : (progress >= 75 ? "rgbrgb(6, 100, 6)" : (progress >= 50 ? "rgb(12, 129, 31)" : (progress >= 25 ? "rgb(85, 155, 78)" : (progress > 0 ? "rgba(175, 226, 108, 0.658)" : "rgb(211, 211, 211)"))))}}
+    // 파란버전
+    // style={{ backgroundColor: progress >= 95 ? "rgb(17, 73, 17)" : (progress >= 75 ? "rgbrgb(6, 100, 6)" : (progress >= 50 ? "rgb(12, 129, 31)" : (progress >= 25 ? "rgb(85, 155, 78)" : (progress > 0 ? "rgba(175, 226, 108, 0.658)" : "rgb(211, 211, 211)"))))}}
+    // 보라버전
+    style={{ backgroundColor: progress >= 95 ? "rgb(100, 7, 92)" : (progress >= 75 ? "rgba(168, 60, 148, 0.911)" : (progress >= 50 ? "rgba(192, 107, 178, 0.863)" : (progress >= 25 ? "rgb(202, 136, 191)" : (progress > 0 ? "rgba(248, 194, 248, 0.808)" : "rgb(211, 211, 211)"))))}}
+    // 갈색버전
+    // style={{ backgroundColor: progress >= 95 ? "rgba(102, 53, 7, 0.904)" : (progress >= 75 ? "rgba(134, 73, 17, 0.836)" : (progress >= 50 ? "rgba(138, 93, 51, 0.904)" : (progress >= 25 ? "rgba(173, 135, 96, 0.945)" : (progress > 0 ? "rgb(207, 180, 154)" : "rgb(211, 211, 211)"))))}}
+    // 베이지버전
+    // style={{ backgroundColor: progress >= 95 ? "rgb(171, 131, 89)" : (progress >= 75 ? "rgb(170, 144, 117)" : (progress >= 50 ? "rgb(202, 172, 140)" : (progress >= 25 ? "rgb(216, 192, 166)" : (progress > 0 ? "rgb(233, 212, 190)" : "rgb(211, 211, 211)"))))}}
   />
 }
 
